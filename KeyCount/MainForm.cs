e14962d3cb586acbaa7a85746f28b86b16ec50da
Victor <publicdomainvic@gmail.com>
2022-05-11 15:48:30 -0400

    Add OnFreeReleasesPublicDomainisToolStripMenuItemClick()

--- KeyCount/MainForm.cs
+++ KeyCount/MainForm.cs
@@ -177,7 +177,8 @@ namespace KeyCount
         /// <param name="e">Event arguments.</param>
         private void OnFreeReleasesPublicDomainisToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Open our website
+            Process.Start("https://publicdomain.is");
         }
 
         /// <summary>
